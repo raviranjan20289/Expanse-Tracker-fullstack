61427a1815732f7e8cf269267a4cdbe405e4200f
diler20289@gmail.com <diler20289@gmail.com>
2023-04-23 23:40:31 +0530

    token completed

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require('path');
 const bodyParser = require('body-parser')
 
+const User = require('./models/user')
+const Expense = require('./models/expenseModel')
+
 const cors = require('cors')
 
 const app = express();
@@ -17,11 +20,18 @@ const sequelize = require('./utility/database');
 
 const signUpRoutes = require('./routes/user');
 
-const loginRoutes =  require('./routes/login')
+const loginRoutes =  require('./routes/login');
+
+const expenseRoutes = require('./routes/expenseRoutes');
 
 app.use('/user',signUpRoutes);
 
 app.use('/user',loginRoutes);
+
+app.use('/expense',expenseRoutes);
+
+Expense.belongsTo(User)
+User.hasMany(Expense);
 
 sequelize.sync()
 .then(()=>{
---
+++ controller/expenseController.js
@@ -0,0 +1,47 @@
+const User = require('../models/user')
+const Expense = require('../models/expenseModel')
+
+
+
+exports.getExpenses = async (req,res) => {
+    try{
+        // console.log(req.user)
+        const expense= await req.user.getExpenses()
+        return res.status(200).json({expenseData: expense})
+    }catch(err){
+        
+        console.log(err)
+    }
+}
+
+exports.postAddExpense = async (req,res) => {
+    try{
+    console.log(req.user,'get rekt')
+    const item =req.body.item;
+    const expense = req.body.expense;
+    const category = req.body.category;
+    const description = req.body.description
+    
+    const data = await req.user.createExpense({
+        item: item,
+        expense: expense,
+        category: category,
+        description: description,
+        
+    })
+    return res.status(200).json({expenseData: data})
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+
+exports.deleteExpense = async (req,res) =>{
+    try{
+        const expId = req.params.id
+        const resp = await Expense.destroy({where:{id:expId}})
+        res.json({response:resp})
+    }catch(err){
+        console.log(err)
+    }
+}
