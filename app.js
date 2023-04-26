69214d39f4284f2d8004e94f947e3a4e12f132e0
diler20289@gmail.com <diler20289@gmail.com>
2023-04-26 12:11:09 +0530

    forgot

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 
 const User = require('./models/user')
 const Expense = require('./models/expenseModel')
+const Order = require('./models/orderModel')
 const ForgotPass =require('./models/forgotPassModel')
 const cors = require('cors')
 
@@ -24,6 +25,8 @@ const loginRoutes =  require('./routes/login');
 
 const expenseRoutes = require('./routes/expenseRoutes');
 
+const purchaseRoutes = require('./routes/purchaseRoutes')
+
 const premiumRoutes = require('./routes/premiumRoutes');
 
 const forgotPassRoutes = require('./routes/forgotPassRoutes')
@@ -34,12 +37,20 @@ app.use('/user',loginRoutes);
 
 app.use('/expense',expenseRoutes);
 
+app.use('/purchase',purchaseRoutes)
+
 app.use('/premium',premiumRoutes);
 
 app.use('/password',forgotPassRoutes)
 
 Expense.belongsTo(User)
 User.hasMany(Expense);
+
+Order.belongsTo(User)
+User.hasMany(Order);
+
+ForgotPass.belongsTo(User)
+User.hasMany(ForgotPass);
 
 sequelize.sync()
 .then(()=>{
